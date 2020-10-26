6f66a630dafa6f8e44e99e5334e71728a4e8ed5c
wyrdmoon <criscorcoran@gmail.com>
2020-10-26 14:00:59 -0600

    fizzbuzz assignment

--- fizzbuzz.py
+++ fizzbuzz.py
@@ -1,11 +1,16 @@
-def fizzbuzz(checkNumber):
-    if checkNumber % 3 == 0:
-        print("fizz")
-        print(checkNumber)
-    elif checkNumber % 5 == 0:
-        print("buzz")
-        print(checkNumber)
-    elif checkNumber % 3 == 0 and checkNumber % 5 ==0:
-        print ("fizzbuzz")
-        print(checkNumber)
+def number(list):
+  for number in list:  
+    if (number % 3 == 0 and number% 5 == 0):
+      print("FizzBuzz")
+    elif (number % 3 == 0):
+      print("Fizz")
+    elif (number % 5 == 0):
+      print("Buzz")
+    else:
+      print(number)
+
+
+list = [45,22,18,23,50,21,10,3,99,55,34,44,71,30,9,33,71,87,31,20]
+number(list)
+
     
